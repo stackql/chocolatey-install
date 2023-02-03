2f6b538a6f0e8fe2a12fe6f83b942bd539dfdb94
jeffreyaven <javen@avensolutions.com>
2023-02-03 16:55:54 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# chocolatey install for stackql
---
+++ stackql/tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://releases.stackql.io/stackql/latest/stackql_windows_amd64.msi'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'stackql*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/passive /l* stackql.log"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
