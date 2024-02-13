44a5522eca4c6e088f979083cc77fbedb4b40b5a
Jeffrey Aven <javen@avensolutions.com>
2024-02-13 19:20:07 +1100

    v0.5.571

--- stackql/tools/chocolateyinstall.ps1
+++ stackql/tools/chocolateyinstall.ps1
@@ -8,11 +8,12 @@ $packageArgs = @{
     fileType       = 'MSI'
     url64bit       = $url64
     softwareName   = 'stackql*'
-    checksum64     = '22e7b46790ee73abf375923819c36fc0abe5886f049c4ac1f03961d28243e2aa'
+    checksum64     = '70484fb95076335f1f5cf5f6c4dc3dc4c470eeb530322aaf0c49c31d6b4f301c'
     checksumType64 = 'sha256'
     silentArgs     = "/passive /l* stackql.log"
     validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
+
 
