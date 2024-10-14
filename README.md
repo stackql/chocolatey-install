d3d3d65cb602979671668486aa736cb9215252a3
Jeffrey Aven <javen@avensolutions.com>
2024-10-15 10:27:11 +1100

    0.5.748

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ To bump the version, update the `version` and `releaseNotes` fields in `stackql/
 <package xmlns="http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd">
   <metadata>
     <id>stackql</id>
-    <version>0.5.742</version>
-    <releaseNotes>https://github.com/stackql/stackql/releases/tag/v0.5.742</releaseNotes>
+    <version>0.5.748</version>
+    <releaseNotes>https://github.com/stackql/stackql/releases/tag/v0.5.748</releaseNotes>
     ...
 ```
 
--- stackql/tools/chocolateyinstall.ps1
+++ stackql/tools/chocolateyinstall.ps1
@@ -8,13 +8,14 @@ $packageArgs = @{
     fileType       = 'MSI'
     url64bit       = $url64
     softwareName   = 'stackql*'
-    checksum64     = 'a5a5355bf5f103a93b8c04995c432f18488643064daa4661742d3d499098688c'
+    checksum64     = '496c817aa0c360386786de6ae9ff47d9f1d735c464649f9639cda43f8d4aeef7'
     checksumType64 = 'sha256'
     silentArgs     = "/passive /l* stackql.log"
     validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
+
 
 
 
